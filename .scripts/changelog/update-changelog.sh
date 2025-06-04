ac1a1120417bb77fb02e95b051fa8e47b0a09362
Zoë Gidiere <duplexsys@protonmail.com>
2025-06-03 23:47:15 -0600

    Update update-changelog.sh

--- .scripts/changelog/update-changelog.sh
+++ .scripts/changelog/update-changelog.sh
@@ -2,8 +2,8 @@
 
 # Moves unreleased section to a new version release section, and updates anchors at the bottom of the changelog
 
-source ../lib.sh
-source ../vars.sh
+source ./.scripts/lib.sh
+source ./.scripts/vars.sh
 ensure_env "version"
 ensure_env "previous_version"
 ensure_env "repo_url"
